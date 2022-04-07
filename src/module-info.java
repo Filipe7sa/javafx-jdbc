2932ed7a768dad0acc4da41bf0eb7afdc803a68b
Filipe Souza <filipe17souza@gmail.com>
2022-04-07 03:49:36 -0300

    DepartmentService

--- src/module-info.java
+++ src/module-info.java
@@ -2,9 +2,11 @@ module workshop{
 	
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires javafx.base;
 	
 	
 	opens application to javafx.graphics, javafx.fxml, javafx.controls;
 	opens gui to javafx.graphics, javafx.fxml, javafx.controls;
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.controls, javafx.base;
 	
 }
